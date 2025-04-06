19ee3eee630a35eacae8745a1dd42245ab069024
NitinVibe <mehul.indianit@gmail.com>
2025-04-06 18:46:54 +0530

    done again

--- gitpro.py
+++ gitpro.py
@@ -1,9 +1,9 @@
+# this is an odd even program
+
 a=int(input("enter a digit to find it is even or odd"))
 
 if(a%2==0):
-    print("it is even number")
+     print("it is even number")
 else:
-    print("it's an odd number")
-
-
-print("hi bro")
+     print("it's an odd number")
+         
