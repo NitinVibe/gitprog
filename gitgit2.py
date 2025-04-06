3084862c397034401c05175da29d7c7a144194c3
NitinVibe <mehul.indianit@gmail.com>
2025-04-06 19:03:32 +0530

    done done

--- gitgit2.py
+++ gitgit2.py
@@ -1,10 +1,10 @@
-for i in range(2,50):
-    j= 2
-    while(j<=(i/2)):
-        if(i%j==0):
-            break
-        j +=1
-    if(j > (i/j)):
-         print(i,"is a prime number")
-         break
+for i in range(2, 50):
+    is_prime = True  # Assume the number is prime initially
+    for j in range(2, int(i**0.5) + 1):  # Optimized loop up to the square root of i
+        if i % j == 0:
+            is_prime = False  # Found a divisor, so it's not prime
+            break  # No need to check further divisors for this number
+    if is_prime:
+        print(i, "is a prime number")
+
 print("byy byy !")
